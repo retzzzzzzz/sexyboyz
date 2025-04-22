02fa7a45063a46bb912d2456dfa5adde4c3504a4
retzzinho <101991392+retzzzzzzz@users.noreply.github.com>
2025-04-21 22:58:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -64,14 +64,14 @@ document.addEventListener("DOMContentLoaded", () => {
         particlesJS('particles-js', {
             particles: {
                 number: { value: 100, density: { enable: true, value_area: 800 } },
-                color: { value: '#ff0004' },
+                color: { value: '#ffffff' },
                 shape: { type: 'circle' },
                 opacity: { value: 0.7 },
                 size: { value: 3 },
                 line_linked: {
                     enable: true,
                     distance: 150,
-                    color: '#ff0004',
+                    color: '#ffffff',
                     opacity: 0.4,
                     width: 1
                 },
